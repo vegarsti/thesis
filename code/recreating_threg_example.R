a9b9ce6d77da70c81a2bff7b79ccba08afb5a687
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2018-02-27 09:37:37 +0100

    Change after supervision comments, add recreated example, syntax highlighting

--- code/recreating_threg_example.R
+++ code/recreating_threg_example.R
@@ -1,12 +1,15 @@
 library(threg)
 data("lkr")
 lkr$f.treatment2=factor(lkr$treatment2)
-# head(lkr)
 fit <- threg(Surv(weeks, relapse) ~ f.treatment2|f.treatment2, data=lkr)
-fit
 
 library(dplyr)
-tbl <- select(.data=lkr, weeks, relapse, f.treatment2)
+#tbl <- select(.data=lkr, weeks, relapse, f.treatment2)
+tbl <- data.frame(lkr$weeks, lkr$relapse, lkr$f.treatment2,
+                  row_names = c("weeks", "relapse", "f.treatment2"))
+names(tbl)[names(tbl) == "lkr.weeks"] <- "weeks"
+names(tbl)[names(tbl) == "lkr.relapse"] <- "relapse"
+names(tbl)[names(tbl) == "lkr.f.treatment2"] <- "f.treatment2"
 
 to_optimize <- function(params) {
   total_loglikelihood <- 0
@@ -20,25 +23,12 @@ to_optimize <- function(params) {
     t_i <- tbl_i$weeks
     is_treated <- as.integer(tbl_i$f.treatment2)-1
     X_i <- c(1, is_treated)
-    #print(X_i)
-    #print(tbl_i)
-    #print(tbl_i$f.treatment2)
-    #print(X_i)
-    #print("")
-    print(gamma*X_i)
     y0_i <- exp(sum(gamma*X_i))
-    print(y0_i)
     mu_i <- sum(beta*X_i)
     log_f_i <- log(y0_i) - 0.5*log(2*pi*t_i^3) - ((y0_i + mu_i*t_i)^2)/(2*t_i)
     log_S_i <- log(1 - pnorm(-(y0_i+mu_i*t_i)/sqrt(t_i)) - exp(-2*y0_i*mu_i)*pnorm((mu_i*t_i-y0_i)/sqrt(t_i)))
     loglik_i <- event*log_f_i + (1 - event)*log_S_i
     total_loglikelihood <- total_loglikelihood + loglik_i
-    #print(loglik_i)
-    #if (is.na(loglik_i)) {
-    # print(loglik_i)
-    # print(tbl_i)
-    #}
-    #print(tbl_i)
   }
   return(-total_loglikelihood)
 }
@@ -48,22 +38,8 @@ threg_value <- -to_optimize(params_from_threg)
 
 initial_params <- c(1, 1, 1, 1)
 
-#to_optimize(initial_params)
-
 best <- nlm(to_optimize, initial_params)
 params_from_best <- best$estimate
 best_value <- -best$minimum
-best_value
-threg_value
-
-
-
-# event <- 1 or 0
-# event <- 1
-# censored <- 1 - event
-# y0_i <- exp(Xt*g)
-# mu_i <- Xt*b
-# log_f_i <- log(y0) - 0.5*log(2*pi*t^3) - (y0 + mu)^2/2*t
-# log_S_i <- log(pnorm(-(y0+mu)/sqrt(t)) + exp(-2*y0*mu)*pnorm((mu*t-y0)/sqrt(t)))
-
-# log_likelihood <- sum(event*log_f_i + censored*log_S_i)
+print(best_value)
+print(threg_value)
